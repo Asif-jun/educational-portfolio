f0713b3cb07526aeba879030dd5992db376a3a65
asifabbasov <aassifabbasov@gmail.com>
2025-01-17 17:42:32 +0400

    testimony2diz

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -3,13 +3,16 @@ import { SectionTitle } from '../../../components/SectionTitle'
 import { Icon } from '../../../components/icon/Icon'
 import { Slider } from '../../../components/slider/Slider'
 import { FlexWrapper } from '../../../components/FlexWrapper'
+import { IconWrapper } from '../skills/skill/Skill'
 
 export const Testimony = () => {
 	return (
 		<StyledTestimony>
 			<SectionTitle>Testimony</SectionTitle>
 			<FlexWrapper direction={'column'} align={'center'}>
-				<Icon iconId={'quote'} />
+				<IconWrapper>
+					<Icon iconId={'quote'} />
+				</IconWrapper>
 				<Slider />
 			</FlexWrapper>
 		</StyledTestimony>
@@ -18,4 +21,9 @@ export const Testimony = () => {
 
 const StyledTestimony = styled.section`
 	background-color: #aeb9ff;
+	min-height: 50vh;
+
+	${IconWrapper} {
+		margin: 30px 0 72px;
+	}
 `
